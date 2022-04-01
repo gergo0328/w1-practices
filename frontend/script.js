be5210d935ea6ef8fb2783476332f0dc703f15a9
gergo0328 <gergo0328@gmail.com>
2022-04-01 12:24:57 +0200

    script.js

---
+++ frontend/script.js
@@ -0,0 +1,26 @@
+console.log("ez az első javascriptem")
+/*
+//idézőjelben kell tenni a stringetket (szövegeket)
+
+"ez itt egy szöveg"
+//dupla" " és sima'' macskaköröm csak egy soros stringet enged meg
+'ez itt egy másik szöveg'
+
+//backtickek közé tett stingekben lehet sortörés
+`ez egy több 
+
+
+soros szöveg(string)`
+
+//a számokat nem kell idézőjelbe tenni.
+4
+128
+04
+1.13
+*/
+
+function myFirstFunction(){
+    console.log("Ez az első függvényem belsejében levő log")
+}
+
+myFirstFunction()
